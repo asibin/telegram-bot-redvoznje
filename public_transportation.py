f5cc2606e495394b18a1ed34e69aaf02a81186c1
Sibin Arsenijevic <sarsenijevic@gogoair.com>
2016-09-02 11:06:47 +0200

    testing markdown

--- public_transportation.py
+++ public_transportation.py
@@ -1,4 +1,7 @@
 import logging
+
+from telegram import ParseMode
+
 from utils import BG_TRAIN_MAPPING, BG_TRAIN_URL, DOMAIN, MINIBUS_URL, BUS_URL, NIGHT_BUS_URL
 from utils import log_updates
 
@@ -10,9 +13,12 @@ def get_bus_schedule(bot, update, args):
     LOG.debug('Passed arguments: {}'.format(args))
     if len(args) == 1:
         req = args[0]
+
         for direction in [1, 2]:
             if req.upper().startswith('E'):  # This is a minibus line
-                url = MINIBUS_URL.format(DOMAIN, req.lower(), direction)
+                url = "[{} direction {}]({})".format(req.upper(),
+                                                     direction,
+                                                     MINIBUS_URL.format(DOMAIN, req.lower(), direction))
 
             elif req.upper().endswith('N'):
                 req = req[:-1]  # Strip out N
@@ -27,7 +33,7 @@ def get_bus_schedule(bot, update, args):
                 url = BUS_URL.format(DOMAIN, req.lower(), direction)
 
             LOG.debug("Request URL is: {}".format(url))
-            bot.sendMessage(chat_id=update.message.chat_id, text=url)
+            bot.sendMessage(chat_id=update.message.chat_id, text=url,  parse_mode=ParseMode.MARKDOWN)
     else:
         LOG.warning('Bus number is required! Arguments passed: {}'.format(args))
         bot.sendMessage(chat_id=update.message.chat_id, text='Bus number is required, i.e. /bus 53')
@@ -37,12 +43,13 @@ def get_bus_schedule(bot, update, args):
 def get_train_schedule(bot, update, args):
     LOG.debug('Passed arguments: {}'.format(args))
     text = 'Allowed values are:\n{}'.format("\n".join(BG_TRAIN_MAPPING.iterkeys()))
+
     if len(args) == 1:
         try:
             direction = BG_TRAIN_MAPPING[args[0]]
             url = BG_TRAIN_URL.format(DOMAIN, direction)
             LOG.debug("Generated BG_TRAIN url: {}".format(url))
-            bot.sendMessage(chat_id=update.message.chat_id, text=url)
+            bot.sendMessage(chat_id=update.message.chat_id, text=url, parse_mode=ParseMode.MARKDOWN)
         except KeyError:
             LOG.debug("Requested arguments: {}".format(args[0]))
             bot.sendMessage(chat_id=update.message.chat_id, text=text)
